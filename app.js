a9c7351917024d12c7e74e859406e957218942d6
Gibbs <awsjg@f8ffc224ffd0.ant.amazon.com>
2020-10-06 21:21:34 +1100

    Full pipeline test 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Solar System!'));
+app.get('/', (req, res) => res.send('Hello Solar System!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
